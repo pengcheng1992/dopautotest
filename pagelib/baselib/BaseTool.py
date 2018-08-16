b827f8a37661781783fd75306445b028e5627700
pengcheng <pengcheng@hujiang.com>
2018-08-16 20:44:12 +0800

    增加了环境配置以及部分日志功能

--- pagelib/baselib/BaseTool.py
+++ pagelib/baselib/BaseTool.py
@@ -5,35 +5,54 @@ from selenium import webdriver
 from conf.EnvConf import sysconf as sc
 from pagelib.baselib import Logger
 import json
-
+from pagelib.baselib.html_logger import setup,info,err,dbg,warn
 
 
 class BaseTool(object):
 
+    driver = None
     def open_browser(self,url,msg=None):
         print(msg)
         if  (sc.browser == "chrome"):
             # 打开谷歌浏览器
-            driver = webdriver.Chrome()
+            try:
+                self.driver = webdriver.Chrome()
+                info("打开谷歌浏览器")
+            except:
+                err("打开谷歌浏览器失败")
         elif (sc.browser == "firefox"):
             # 打开火狐浏览器
-            driver = webdriver.Firefox()
+            try:
+                self.driver = webdriver.Firefox()
+                info("打开火狐浏览器")
+            except:
+                err("打开火狐浏览器失败")
         elif (sc.browser == "ie"):
             # 打开IE浏览器
-            driver = webdriver.Ie()
+            try:
+                self.driver = webdriver.Ie()
+                info("打开IE浏览器")
+            except:
+                err("打开IE浏览器失败")
         else:
-            raise "不支持的浏览器！"
+            err("不支持的浏览器")
+
 
         # 窗口最大化
-        driver.maximize_window();
-
+            self.driver.maximize_window();
 
         # 跳转到URL
         try:
-            driver.get(url)
-
+            self.driver.get(url)
         except:
             print("跳转URL失败，请检查URL是否正确！")
+
+    # 获取网页截图
+    def pic_shot(self,filename):
+        try:
+            self.driver.save_screenshot(filename)
+        except:
+            err("截图失败")
 
     # 获取字典数据
     def get_data(self,data_dict,env=None):
@@ -41,19 +60,18 @@ class BaseTool(object):
         if env == None and sc.env in sc.envlist:
             try:
                 value = data_dict.get(sc.env)
-                print(value)
                 return value
             except:
-                print("不支持的数据格式，请使用python字典格式！")
+                err("不支持的数据格式，请使用python字典格式！")
         elif env !=None and env in sc.envlist:
             try:
                 value = data_dict.get(env)
 
                 return value
             except:
-                print("不支持的数据格式，请使用python字典格式！")
+                err("不支持的数据格式，请使用python字典格式！")
         else:
-            raise  "环境配置不正确，请检查环境配置，仅支持【qa】、【yz】、【prod】三种环境！"
+            err("环境配置不正确，请检查环境配置，仅支持【qa】、【yz】、【prod】三种环境！")
 
 
 
